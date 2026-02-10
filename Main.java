608a5c627e0daeb84e4c40611c2865ec422645a8
angiel2005 <angieluo05@gmail.com>
2026-02-09 23:33:15 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Pokemon pikachu = new Pokemon("Pikachu", "Electric");
+        Pokemon charmander = new Pokemon("Charmander", "Fire");
+
+        System.out.println(pikachu);
+        System.out.println(charmander);
+    }
+}
