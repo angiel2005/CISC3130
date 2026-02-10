7daef867fc7477c9361c0bd2fc1b2cf7bd5aa45d
angiel2005 <angieluo05@gmail.com>
2026-02-09 23:27:17 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+package day1.genericLab;
+
+public class main {
+    public static void main(String [] args){
+
+        pokemon pikachu = new pokemon("Pikachu", "Electric");
+        pokemon charmander = new pokemon("Charmander", "Fire");
+        
+        System.out.println(pikachu);
+        System.out.println(charmander);
+        
+    }
+}
