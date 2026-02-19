3e58e704b47d05b06ae2831e80100960fa7f5bec
angiel2005 <angieluo05@gmail.com>
2026-02-19 15:44:56 +0000

    commit Pair.java

--- Main.java
+++ Main.java
@@ -5,5 +5,24 @@ public class Main {
 
         System.out.println(pikachu);
         System.out.println(charmander);
+
+        PokeBox box = new PokeBox(pikachu.getName());
+        System.out.println(box.get());
+        box = new PokeBox(charmander.getName());
+        System.out.println(box.get());
+
+        Box<Pokemon> pokemonBox = new Box<>(pikachu);
+        Box<String> itemBox = new Box<>("Oran Berry");
+        Box<Integer> levelBox = new Box<>(25)
+        
+        Pair<Pokemon, String> heldItem = new Pair<>(pikachu, "Oran Berry");
+        System.out.println(heldItem);
+
+        PokemonOnlyBox<Pokemon> b1 = new PokemonOnlyBox<>(pikachu);
+        System.out.println(b1.getType());
+
+        BattlePokemon squirtle = new BattlePokemon("Squirtle", "Water", 60);
+        PokemonOnlyBox<BattlePokemon> battleBox = new PokemonOnlyBox<>(squirtle);
+        System.out.println(battleBox.get());
     }
 }
